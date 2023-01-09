7977ad97df6840c85fadb2c3a15a02942452896d
sholabash <sholabash106@gmail.com>
2023-01-09 12:54:42 +0100

    first commit

---
+++ lib/categories_screen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import './dumy_data.dart';
+import './category_item.dart';
+
+// ignore: use_key_in_widget_constructors
+class CategoriesScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('DeliMeal'),
+      ),
+      body: GridView(
+        // ignore: sort_child_properties_last
+        children: DUMMY_CATEGORIES
+            .map(
+              (catData) => CategoryItem(
+                catData.title,
+                catData.color,
+              ),
+            )
+            .toList(),
+        // ignore: prefer_const_constructors
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/category_item.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class CategoryItem extends StatelessWidget {
+  late final String title;
+  late final Color color;
+
+  CategoryItem(this.title, this.color);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(15),
+      child: Text(title),
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            color.withOpacity(0.7),
+            color,
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+        borderRadius: BorderRadius.circular(15),
+      ),
+    );
+  }
+}
